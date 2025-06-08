370faed38fd6fab9355302793222ffe3cef4ca46
manik08125 <manikchavan56jaddu@gmail.com>
2025-06-08 21:09:12 +0530

    Add files via upload

---
+++ dice roller. Script.js
@@ -0,0 +1,11 @@
+﻿const rollBtn = document.getElementById("rollBtn");
+const diceFace = document.getElementById("diceFace");
+
+const diceFaces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+
+rollBtn.addEventListener("click", function () {
+
+    const randomIndex = Math.floor(Math.random() * 6);
+    const face = diceFaces[randomIndex];
+    diceFace.textContent = face;
+});
